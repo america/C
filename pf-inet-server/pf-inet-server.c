33cb7c4187ebd1a825fa1bb0fa641677b360718a
america <dreamers.ball66@gmail.com>
2014-01-13 10:45:06 +0900

    pf-inet-server add

---
+++ pf-inet-server/pf-inet-server.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include "pf-inet-server.h"
+
+int main(int argc, char *argv[])
+{
+  int server_d;                                 /* ソケットディスクリプタ */
+  struct sockaddr_in server_addr;               /* サーバのアドレス */
+  struct sockaddr_in client_addr;               /* クライアントのアドレス */
+  int client_len = sizeof(struct sockaddr_in);  /* 上の長さ */
+  char recv_buf[BUFSIZ];                        /* 受信バッファ */
+
+  if(argc != 2){
+    fprintf(stderr,"Usage: %s server-port\n",argv[0]);
+    exit(EXIT_FAILURE);
+  }
+
+  /* アドレスの初期化 */
+  /* サーバのアドレス */
+  server_addr.sin_family = PF_INET;
+  /* 接続を受け付けるアドレス(つまりどのNICで接続を受け付けるか)
+     ここでは全てのアドレスでの接続を受け付けている */
+  server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  server_addr.sin_port = htons(atoi(argv[1]));
+
+  /* ソケットの作成 */
+  errno = 0;
+  if((server_d = socket(PF_INET, SOCK_DGRAM, 0)) == -1){
+    MY_ABORT("socket failure");
+  }
+
+  /* ソケットのbind */
+  errno = 0;
+  if(bind(server_d, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1){
+    MY_ABORT("bind failure");
+  }
+
+  fprintf(stdout,"starting server: port = %d\n", ntohs(server_addr.sin_port));
+
+  /* 受信 */
+  while(1){
+    errno = 0;
+    if(recvfrom(server_d,                        /* ソケットディスクリプタ */
+		recv_buf,                        /* 受信バッファ */
+		BUFSIZ,                          /* 受信バッファサイズ */
+		MSG_NOSIGNAL,                    /* オプション */
+		(struct sockaddr *)&client_addr, /* クライアントのアドレス */
+		&client_len                      /* 上のサイズ */
+		) == -1){
+      MY_ABORT("recvform failure");
+    }
+    
+    /* サーバ処理 */
+    fprintf(stdout, "recved msg from %s:%d\n",
+	    inet_ntoa(client_addr.sin_addr),
+	    ntohs(client_addr.sin_port));
+    fprintf(stdout,"recv msg: \"%s\"\n",recv_buf);
+    if(strcmp(recv_buf, "exit") == 0){
+      break;
+    }else{
+      memset(recv_buf, '\0', BUFSIZ);
+    }
+  } 
+
+ /* ソケットのclose */
+ close(server_d);
+ 
+ exit(EXIT_SUCCESS);
+
+}
