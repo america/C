9057cc3f4d6dbec2e97675f67d91485b9f95c4f1
america <dreamers.ball66@gmail.com>
2014-01-13 11:00:19 +0900

    pf-inet-client.h

---
+++ pf-inet-client/pf-inet-client.h
@@ -0,0 +1,10 @@
+#ifndef _PF_INET_SERVER_H
+#define _PF_INET_SERVER_H 
+ 
+#include <stdlib.h>
+
+#define MY_ABORT(__COMMENT__) \
+  fprintf (stderr, "\n" __FILE__ "(%d) " __COMMENT__ "\n", __LINE__); \
+  exit (EXIT_FAILURE);
+
+#endif
