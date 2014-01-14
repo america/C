a111e0055313e801d728991b57600c5dd6f91a46
america <dreamers.ball66@gmail.com>
2014-01-15 00:41:05 +0900

    simple phone book

---
+++ phone1/mysub.h
@@ -0,0 +1,8 @@
+#ifndef _MYSUB_H
+#define _MYSUB_H
+
+int getint(char *);
+double getdouble(char *);
+void getstr(char *, char *, int);
+
+#endif
---
+++ phone1/phone1.c
@@ -0,0 +1,101 @@
+#include <sys/types.h> /* lseed, open, read, while */
+#include <stdio.h>     /* fprintf, perror, printf */
+#include <stdlib.h>    /* exit */
+#include <string.h>    /* memset */
+#include <sys/stat.h>  /* open */
+#include <fcntl.h>     /* open */
+#include <sys/uio.h>   /* read, write */
+#include <unistd.h>    /* close, lseek, read, write */
+#include "mysub.h"
+
+#define PHONEBOOK "phone1.dat" /* 電話帳ファイルの名前 */
+
+enum{
+  RECLEN = 32,                 /* ファイル内での１項目の長さ */
+};
+
+/* 関数のプロトタイプ宣言 */
+void show(int);
+void input(int);
+
+int main(void){
+  int fd;
+  int cmd, finish;
+
+  /* show では読み、input では書くので、読み書きで開く.
+   * ファイルがなければ作るが, 既にファイルがある場合は
+   * その内容を消してはいけないので, O_TRUNC は指定しない
+   */
+  if((fd = open(PHONEBOOK, O_RDWR|O_CREAT, 0666)) < 0){
+    perror("open");
+    exit(EXIT_FAILURE);
+  }
+  finish = 0;
+  while(!finish){
+    cmd = getint("command (0=show, 1=input, 2=end)? ");
+    switch(cmd){
+      case 0:
+        show(fd);
+        break;
+      case 1:
+        input(fd);
+        break;
+      case 2:
+        /* 繰り返しを抜ける */
+        finish = 1;
+        break;
+      default:
+        fprintf(stderr, "unknown command %d\n", cmd);
+        break;
+    }
+  }
+  if(close(fd) < 0){
+    perror("close");
+    exit(EXIT_FAILURE);
+  }
+  return 0;
+}
+
+/* 表示部 */
+void show(int fd){
+  int recno;
+  ssize_t cc;
+  char rec[RECLEN+1]; /* +1はヌル文字のため */
+
+  rec[RECLEN] = '\0'; /* あらかじめヌル文字を入れておく */
+  recno = getint("record number? ");
+  /* recno * RECLEN が, ファイルにおける項目 recno の先頭位置 */
+  if(lseek(fd, recno * RECLEN, SEEK_SET) == -1){
+    perror("lseek");
+    return;
+  }
+  if((cc = read(fd, rec, RECLEN)) < 0){
+    perror("read");
+    return;
+  }
+  if(cc == 0){
+    printf("no item\n");
+  }else{
+    printf("item = \"%s\"\n", rec);
+  }
+  return;
+}
+
+/* 入力部 */
+void input(int fd){
+  int recno;
+  char rec[RECLEN+1];
+  
+  memset(rec, 0, sizeof(rec));
+  recno = getint("record number? ");
+  if(lseek(fd, recno * RECLEN, SEEK_SET) == -1){
+    perror("lseek");
+    return;
+  }
+  getstr("name? ", rec, sizeof(rec));
+  if(write(fd, rec, RECLEN) != RECLEN){
+    perror("write");
+    return;
+  }
+  return;
+}
