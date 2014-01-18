60bda02fae722e6432e8b3018ba8d5d386b4b3e3
america <dreamers.ball66@gmail.com>
2014-01-18 19:15:49 +0900

    clockによる処理時間の計算を追加

--- quicksort/main.c
+++ quicksort/main.c
@@ -1,24 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+#include "quicksort.h"
 
+#define LENGTH 2000000
+ 
 int main(void)
 {
-  int data[] = {2, 9, 7, 100, 77, 22, 11, 82, 33, 66};
+  int data[LENGTH];
   int i;
 
+  clock_t start, stop;
+  double diff;
+
+  for(i = 0; i < LENGTH; i++){
+    data[i] = rand();
+  }
+
+  /*
   fprintf(stdout, "before\n");
 
-  for(i = 0; i < 10; i++){
-    fprintf(stdout, "%d\n", data[i]);
+  for(i = 0; i < LENGTH; i++){
+    fprintf(stdout, "%d,", data[i]);
   }  
+  fprintf(stdout, "\n");
+  */
 
-  /*quick_sort(data, sizeof(data));*/
-  quick_sort(data, 10);
+  fprintf(stdout, "----------\n");
+  start = clock();
+  quick_sort(data, LENGTH);
+  stop = clock();
 
+  diff = (double)(stop - start) / CLOCKS_PER_SEC;
+  fprintf(stdout, "clock 関数 で計測: %lf[sec]\n", diff);
+
+  fprintf(stdout, "----------\n");
+
+  /*
   fprintf(stdout, "after\n");
 
-  for(i = 0; i < 10; i++){
-    fprintf(stdout, "%d\n", data[i]);
+  for(i = 0; i < LENGTH; i++){
+    fprintf(stdout, "%d,", data[i]);
   }  
+  */
 
   return 0;
 }
---
+++ quicksort/quicksort.h
@@ -0,0 +1,6 @@
+#ifndef _QUICKSORT_H
+#define _QUICKSORT_H
+
+void quick_sort(int *data, int data_size);
+
+#endif
